889a47393f589b48e2143cff87a30bcfdc9d2def
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-27 15:11:22 -0500

    will fix later

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,70 +1,71 @@
-import React from 'react';
-import {Link } from 'react-router-dom'
-import {connect} from 'react-redux'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class LoginPage extends React.Component {
-    constructor(props){
-        super(props)
-        this.state= {
-            username: '',
-            password: '',
-            
-        }
-
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleUserChange = this.handleUserChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
     }
 
-    handleUserChange(event) {
-        this.setState({
-          username: event.target.value,
-        });
-      };
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleUserChange = this.handleUserChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-   handlePasswordChange(event) {
+  handleUserChange(event) {
+    this.setState({
+      username: event.target.value,
+    })
+  }
+
+  handlePasswordChange(event) {
     this.setState({
       password: event.target.value,
-    });
+    })
   }
 
   handleSubmit(event) {
-      event.preventDefault();
-      if (!this.state.username) {
-        alert("please enter username")
-      }
-  
-      if (!this.state.password) {
-        alert("please enter password")
-      }
-  
-      
+    event.preventDefault()
+    if (!this.state.username) {
+      alert('please enter username')
+    }
+
+    if (!this.state.password) {
+      alert('please enter password')
     }
   }
 
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>UserName</label>
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={this.handleUserChange}
+        />
 
-    render(){
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <label>UserName</label>
-                    <input type="text" value={this.state.username} onChange={this.handleUserChange} />
-
-                <label>Password</label>
-                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
-                    <button onClick = {this.handleSubmit}>Submit</button>
-
-
-
-            </form>
-        )
-        
+        <label>Password</label>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <button onClick={this.handleSubmit}>Submit</button>
+      </form>
+    )
+  }
 }
 
-function mapState(state){
-    return
+function mapState(state) {
+  return
 }
 
-const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
+// const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
 
-export {connectedLoginPage as LoginPage};
+// export {connectedLoginPage as LoginPage};
 
+export default LoginPage
