eb3ce0fb9eb401d797aa411914baebaa07e1d14e
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-28 22:03:08 -0500

    add styling to traits

--- src/components/races/Trait.jsx
+++ src/components/races/Trait.jsx
@@ -5,6 +5,8 @@ import { getApiData } from '../../redux/reducers'
 
 import { Link } from 'react-router-dom'
 
+import '../../css/click-away.css'
+
 class Trait extends Component {
   state = {
     raceThatCalled: this.props.trait, // holds the race that called this page before redux state is overwritten
@@ -23,44 +25,63 @@ class Trait extends Component {
     console.log('url param:', this.props.match.params.trait)
     return (
       <div>
-        {/* If page refreshes, then race that called this is undefined b/c its overwritten, thus it shows nothing */}
-        {this.state.raceThatCalled.index !== undefined ? (
-          <Link to={`/races/${this.state.raceThatCalled.index}`}>
-            <p>Back to {this.state.raceThatCalled.name} information</p>
-          </Link>
-        ) : (
-          <span />
-        )}
+        <div className="click-away-container">
+          <div className="back-to">
+            {/* If page refreshes, then race that called this is undefined b/c its overwritten, thus it shows nothing */}
+            {this.state.raceThatCalled.index !== undefined ? (
+              <Link
+                id="back-to-link"
+                to={`/races/${this.state.raceThatCalled.index}`}
+              >
+                Back to {this.state.raceThatCalled.name} information
+              </Link>
+            ) : (
+              <span />
+            )}
+          </div>
 
-        <h1>Trait: {this.props.trait.name}</h1>
+          <div className="click-away-title">
+            <h1>Trait: {this.props.trait.name}</h1>
+          </div>
 
-        <h3>Description</h3>
-        {this.props.trait.desc !== undefined ? (
-          this.props.trait.desc.map((element, index) => {
-            return (
-              <div key={index}>
-                <p>{element}</p>
-              </div>
-            )
-          })
-        ) : (
-          <span />
-        )}
+          <div className="click-away-info-container">
+            <div className="click-away-info-title">
+              <h3>Description</h3>
+            </div>
+            <div className="click-away-info">
+              {this.props.trait.desc !== undefined ? (
+                this.props.trait.desc.map((element, index) => {
+                  return (
+                    <div key={index}>
+                      <p>{element}</p>
+                    </div>
+                  )
+                })
+              ) : (
+                <span />
+              )}
+            </div>
 
-        <h3>Races Applicable</h3>
-        {this.props.trait.races !== undefined ? (
-          this.props.trait.races.map((element, index) => {
-            return (
-              <div key={index}>
-                <Link to={`/races/${element.index}`}>
-                  <p>{element.name}</p>
-                </Link>
-              </div>
-            )
-          })
-        ) : (
-          <span />
-        )}
+            <div className="click-away-info-title">
+              <h3>Races Applicable</h3>
+            </div>
+            <div className="click-away-info-bits">
+              {this.props.trait.races !== undefined ? (
+                this.props.trait.races.map((element, index) => {
+                  return (
+                    <div key={index}>
+                      <Link to={`/races/${element.index}`}>
+                        <p>{element.name}</p>
+                      </Link>
+                    </div>
+                  )
+                })
+              ) : (
+                <span />
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
